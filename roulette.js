f47ea0d77f56f0ccb09ce7dceb67b615c539be5e
UjjwalSharma2304 <itsujjwal2304@gmail.com>
2025-08-01 18:09:21 +0530

    Update roulette.js
    fixed a bug so that the game doesn't start if you have entered a number outside the given range or left the input blank.

--- roulette.js
+++ roulette.js
@@ -13,7 +13,9 @@ button.onclick=function(){
     document.getElementById("number").textContent=random;
     tryCount++;
     trydisplay.textContent=`TRIES: ${tryCount}`;
-    if(random==Number(input.value)){
+    if(input.value=="" || input.value>10 || input.value<1){
+        window.alert("please enter a number between 1-10 to continue")}
+    else if(random==Number(input.value)){
        win.textContent="YOU WIN!!"
        tryCount=0;
        input.value = "";
